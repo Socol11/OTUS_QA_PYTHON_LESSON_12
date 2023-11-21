455ba529f3c46e17a73fd5f3da1a74c9fdcd4b2b
Roman <dumgym@gmail.com>
2023-11-21 07:55:29 +0300

    Update BasePage.py and MainPage.py

--- page/BasePage.py
+++ page/BasePage.py
@@ -5,7 +5,7 @@
 
 from selenium.webdriver.common.by import By
 
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -15,14 +15,23 @@ class BasePage:
     def __init__(self, browser):
         self.browser = browser
 
-    def element(self, locator: tuple):
-        try:
-            return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(locator))
-        except TimeoutException:
-            raise AssertionError(f"Didn't wait for the {locator} element to be visible!")
+    def get_element(self, locator: tuple, wait: int):
+        return WebDriverWait(self.browser, wait).until(EC.visibility_of_element_located(locator))
 
-    def elements(self, locator: tuple):
+    def get_elements(self, locator: tuple, wait: int):
         try:
             return WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located(locator))
         except TimeoutException:
             raise AssertionError(f"Didn't wait for the {locator} elements to be visible!")
+
+    def type_text_action(self, locator, text_type):
+        WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(locator)).send_keys(text_type)
+
+    def click_action(self, locator):
+        WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(locator)).click()
+
+    def get_element_text(self, locator):
+        return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(locator)).text()
+
+    def get_page_title(self):
+        return self.browser.title
---
+++ page/MainPage.py
@@ -0,0 +1,15 @@
+from page.BasePage import BasePage
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class MainPage(BasePage):
+
+    SLIDER = (By.CSS_SELECTOR, ".slideshow")
+
+    def get_slider(self, wait):
+        try:
+            self.get_element(MainPage.SLIDER, wait)
+        except TimeoutException:
+            raise AssertionError(f"Слайдер отсутствует на странице")
