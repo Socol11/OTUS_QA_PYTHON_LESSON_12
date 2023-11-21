4c05775f75bd17b3b8d7793951e4992f60f60cb0
Roman <dumgym@gmail.com>
2023-11-21 09:10:28 +0300

    Update tests for Main page

--- page/MainPage.py
+++ page/MainPage.py
@@ -1,3 +1,4 @@
+import random
 from page.BasePage import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
@@ -7,7 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 class MainPage(BasePage):
 
     SLIDER = (By.CSS_SELECTOR, ".slideshow")
-    SLIDER_PAGINATION = (By.CSS_SELECTOR, ".swiper-pagination4")
+    SLIDER_PAGINATION = (By.CSS_SELECTOR, ".swiper-pagination")
+    PRODUCT_LAYOUT = (By.CSS_SELECTOR, ".product-layout")
+    CAROUSEL = (By.CSS_SELECTOR, ".carousel")
+    BUTTON_GROUP = (By.CSS_SELECTOR, ".button-group")
 
     def get_slider(self, wait):
         try:
@@ -20,3 +24,27 @@ class MainPage(BasePage):
             self.get_element(MainPage.SLIDER_PAGINATION, wait)
         except TimeoutException:
             raise AssertionError(f"Пагинация слайдера отсутствует на странице")
+
+    def get_product_layout(self, wait):
+        product_layout_list = self.get_elements(MainPage.PRODUCT_LAYOUT, wait)
+        try:
+            assert len(product_layout_list) > 0
+        except TimeoutException:
+            raise AssertionError(f"Блоки с товарами отсутствует на странице")
+
+    def get_carousel(self, wait):
+        try:
+            self.get_element(MainPage.CAROUSEL, wait)
+        except TimeoutException:
+            raise AssertionError(f"Карусель отсутствует на странице")
+
+    def get_buttons_in_product_layout(self, wait):
+        product_layout_list = self.get_elements(MainPage.PRODUCT_LAYOUT, wait)
+        # Выбираем случайное число из диапазона от 0 до количества продуктов
+        number = random.randint(0, len(product_layout_list) - 1)
+        # Выбираем случайный элемент из имеющихся
+        random_product_layout = self.get_elements(MainPage.PRODUCT_LAYOUT, wait)[number]
+        try:
+            random_product_layout.find_element(*MainPage.BUTTON_GROUP)
+        except TimeoutException:
+            raise AssertionError(f"Нет группы кнопок в product layout")
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,22 +1,38 @@
-import random
 from page.MainPage import MainPage
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestMainPage:
 
     def test_presence_of_a_slider(self, browser, url, wait=5):
+        """This test checks if the slider is present on the main page"""
         browser.get(url)
         main_page = MainPage(browser)
         main_page.get_slider(wait)
 
     def test_presence_of_slider_pagination(self, browser, url, wait=5):
+        """This test checks if the slider pagination element is present on the main page"""
         browser.get(url)
         main_page = MainPage(browser)
         main_page.get_slider_pagination(wait)
+
+    def test_presence_of_product_layout(self, browser, url, wait=5):
+        """This test checks if the block of products is present on the main page"""
+        browser.get(url)
+        main_page = MainPage(browser)
+        main_page.get_product_layout(wait)
+
+    def test_presence_of_carousel(self, browser, url, wait=5):
+        """This test checks if the carousel of brands is present on the main page"""
+        browser.get(url)
+        main_page = MainPage(browser)
+        main_page.get_carousel(wait)
+
+    def test_presence_of_buttons_in_product_layout(self, browser, url, wait=5):
+        """This test randomly checks if the group of buttons is present in a product layout"""
+        browser.get(url)
+        main_page = MainPage(browser)
+        main_page.get_buttons_in_product_layout(wait)
+
 
 
 # def test_presence_of_a_slider(browser, url):
@@ -37,30 +53,30 @@ class TestMainPage:
 #         raise AssertionError(f"Пагинация слайдера отсутствует на странице")
 
 
-def test_presence_of_product_layout(browser, url):
-    browser.get(url)
-    wait = WebDriverWait(browser, 5)
-    product_layout_list = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".product-layout")))
-    assert len(product_layout_list) > 0
+# def test_presence_of_product_layout(browser, url):
+#     browser.get(url)
+#     wait = WebDriverWait(browser, 5)
+#     product_layout_list = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".product-layout")))
+#     assert len(product_layout_list) > 0
 
 
-def test_presence_of_buttons_in_product_layout(browser, url):
-    browser.get(url)
-    wait = WebDriverWait(browser, 5)
-    product_layout_list = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".product-layout")))
-    number = random.randint(0, len(product_layout_list)-1)
-    # Выбираем случайный элемент из имеющихся
-    random_product_layout = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".product-layout")))[number]
-    try:
-        random_product_layout.find_element(By.CSS_SELECTOR, ".button-group")
-    except TimeoutException:
-        raise AssertionError(f"Нет группы кнопок в product layout")
+# def test_presence_of_buttons_in_product_layout(browser, url):
+#     browser.get(url)
+#     wait = WebDriverWait(browser, 5)
+#     product_layout_list = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".product-layout")))
+#     number = random.randint(0, len(product_layout_list)-1)
+#     # Выбираем случайный элемент из имеющихся
+#     random_product_layout = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".product-layout")))[number]
+#     try:
+#         random_product_layout.find_element(By.CSS_SELECTOR, ".button-group")
+#     except TimeoutException:
+#         raise AssertionError(f"Нет группы кнопок в product layout")
 
 
-def test_presence_of_carousel(browser, url):
-    browser.get(url)
-    wait = WebDriverWait(browser, 5)
-    try:
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".carousel")))
-    except TimeoutException:
-        raise AssertionError(f"Карусель отсутствует на странице")
+# def test_presence_of_carousel(browser, url):
+#     browser.get(url)
+#     wait = WebDriverWait(browser, 5)
+#     try:
+#         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".carousel")))
+#     except TimeoutException:
+#         raise AssertionError(f"Карусель отсутствует на странице")
