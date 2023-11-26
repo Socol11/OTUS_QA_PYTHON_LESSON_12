8999ad040239afe7025f2b4eb6a1e18707e0b595
Roman <dumgym@gmail.com>
2023-11-26 10:53:15 +0300

    Little update for MainPage.py

--- page/MainPage.py
+++ page/MainPage.py
@@ -2,7 +2,6 @@ import random
 from page.BasePage import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class MainPage(BasePage):
