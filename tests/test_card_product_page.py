a727efa85733a4c0e317d8a6fb3656e6c93b075d
Roman <dumgym@gmail.com>
2023-11-26 22:50:12 +0300

    Update test_card_product_page.py - added constants PAGE, WAIT etc.

--- tests/test_card_product_page.py
+++ tests/test_card_product_page.py
@@ -4,41 +4,46 @@ from page.ProductPage import ProductPage
 
 
 class TestCardProductPage:
+    PAGE = '/iphone'
+    WAIT = 5
+    NAME = 'iPhone'
+    BRAND = 'Apple'
+    PRICE = '$123.20'
 
-    def test_thumbnails_presence(self, browser, url, wait=5):
+    def test_thumbnails_presence(self, browser, url, wait=WAIT):
         """This test checks if a product thumbnail is available"""
-        browser.get(url + '/iphone')
+        browser.get(url + self.PAGE)
         product_page = ProductPage(browser)
         product_page.get_thumbnail(wait)
 
-    def test_h1_title_presence(self, browser, url, wait=5, name='iPhone'):
+    def test_h1_title_presence(self, browser, url, wait=WAIT, name=NAME):
         """This test checks if the content of the H1 tag matches the product name"""
-        browser.get(url + '/iphone')
+        browser.get(url + self.PAGE)
         product_page = ProductPage(browser)
         product_page.get_h1_title(wait, name)
 
-    def test_brand_info_presence(self, browser, url, wait=5, brand='Apple'):
+    def test_brand_info_presence(self, browser, url, wait=WAIT, brand=BRAND):
         """This test checks if the content of the brand name element matches the product brand"""
-        browser.get(url + '/iphone')
+        browser.get(url + self.PAGE)
         product_page = ProductPage(browser)
         product_page.get_brand_info(wait, brand)
 
-    def test_price_info_presence(self, browser, url, wait=5, price='$123.20'):
+    def test_price_info_presence(self, browser, url, wait=WAIT, price=PRICE):
         """This test checks if the product price info from H2 tag matches the real product price"""
-        browser.get(url + '/iphone')
+        browser.get(url + self.PAGE)
         product_page = ProductPage(browser)
         product_page.get_price_info(wait, price)
 
-    def test_input_quantity_presence(self, browser, url, wait=5):
+    def test_input_quantity_presence(self, browser, url, wait=WAIT):
         """This test checks if the element for selecting the quantity
         of the product is present on the page"""
-        browser.get(url + '/iphone')
+        browser.get(url + self.PAGE)
         product_page = ProductPage(browser)
         product_page.get_input_quantity(wait)
 
-    def test_add_to_cart_button_presence(self, browser, url, wait=5):
+    def test_add_to_cart_button_presence(self, browser, url, wait=WAIT):
         """This test check if the 'Add to cart' button is present on the page"""
-        browser.get(url + '/iphone')
+        browser.get(url + self.PAGE)
         product_page = ProductPage(browser)
         product_page.get_add_to_cart_button(wait)
 
