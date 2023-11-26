50cb3346067c4f4f8f71bd9732f1168948cfb2da
Roman <dumgym@gmail.com>
2023-11-26 23:04:43 +0300

    Update test_catalogue_page.py - added constants WAIT etc.

--- tests/test_catalogue_page.py
+++ tests/test_catalogue_page.py
@@ -11,6 +11,12 @@ from page.CataloguePage import CataloguePage
 
 
 class TestCataloguePage:
+    WAIT = 5
+    DESKTOPS_PAGE = "/desktops"
+    MP3_PAGE = "/mp3-players"
+    SMARTPHONE_PAGE = "/smartphone"
+    LAPTOP_PAGE = "/laptop-notebook"
+    TABLET_PAGE = "/tablet"
 
     # def test_enter_random_catalogue_page_via_navbar(self, browser, url, wait=5):
     #     """Проверяем возможность попасть в каталог через меню"""
@@ -40,34 +46,33 @@ class TestCataloguePage:
 
     def test_presence_of_catalogue_menu(self, browser, url, wait=5):
         """This test checks if the side catalogue menu is present on the /desktops page"""
-        browser.get(url + "/desktops")
+        browser.get(url + self.DESKTOPS_PAGE)
         catalogue_page = CataloguePage(browser)
         catalogue_page.get_catalogue_menu(wait)
 
-    def test_presence_of_category_img_thumbnail(self, browser, url, wait=5):
+    def test_presence_of_category_img_thumbnail(self, browser, url, wait=WAIT):
         """This test checks if the img thumbnail of the category is present on the /mp3-players page"""
-        browser.get(url + "/mp3-players")
+        browser.get(url + self.MP3_PAGE)
         catalogue_page = CataloguePage(browser)
         catalogue_page.get_category_img_thumbnail(wait)
 
-    def test_presence_of_display_mode_button(self, browser, url, wait=5):
+    def test_presence_of_display_mode_button(self, browser, url, wait=WAIT):
         """This test checks if the display mode buttons of the category are present on the /smartphone page"""
-        browser.get(url + "/smartphone")
+        browser.get(url + self.SMARTPHONE_PAGE)
         catalogue_page = CataloguePage(browser)
         catalogue_page.get_display_mode_button(wait)
 
-    def test_presence_of_sort_by_dropdown(self, browser, url, wait=5):
+    def test_presence_of_sort_by_dropdown(self, browser, url, wait=WAIT):
         """This test checks if the sort_by_dropdown element is present on the /laptop-notebook page"""
-        browser.get(url + "/laptop-notebook")
+        browser.get(url + self.LAPTOP_PAGE)
         catalogue_page = CataloguePage(browser)
         assert catalogue_page.get_sort_by_dropdown(wait).text == "Sort By:"
 
-    def test_presence_of_show_limit_dropdown(self, browser, url, wait=5):
+    def test_presence_of_show_limit_dropdown(self, browser, url, wait=WAIT):
         """This test checks if the show limit dropdown element is present on the /tablet page"""
-        browser.get(url + "/tablet")
+        browser.get(url + self.TABLET_PAGE)
         catalogue_page = CataloguePage(browser)
         assert catalogue_page.get_limit_dropdown(wait).text == "Show:"
-
 
 # def test_enter_random_catalogue_page_via_navbar(browser, url):
 #     """Проверяем возможность попасть в каталог через меню"""
