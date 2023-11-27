f1be6b554785f8b53947cc88f5545765dd9d1823
Roman <dumgym@gmail.com>
2023-11-27 21:04:23 +0300

    Update test_currency_change_for_main_page.py and MainPage.py

--- page/MainPage.py
+++ page/MainPage.py
@@ -12,6 +12,9 @@ class MainPage(BasePage):
     PRODUCT_LAYOUT = (By.CSS_SELECTOR, ".product-layout")
     CAROUSEL = (By.CSS_SELECTOR, ".carousel")
     BUTTON_GROUP = (By.CSS_SELECTOR, ".button-group")
+    PRODUCT_CURRENCY_SYMBOL = (By.CSS_SELECTOR, "p.price")
+    CURRENCY_DROPDOWN_TOGGLE = (By.CSS_SELECTOR, "button.dropdown-toggle")
+    CURRENCY_SELECT_EUR = (By.CSS_SELECTOR, "button.currency-select[name='EUR']")
 
     def get_slider(self, wait):
         try:
@@ -48,3 +51,54 @@ class MainPage(BasePage):
             random_product_layout.find_element(*self.BUTTON_GROUP)
         except TimeoutException:
             raise AssertionError(f"Нет группы кнопок в product layout")
+
+    # def get_default_currency(self, wait):
+    #     # Проверяем валюту по умолчанию
+    #     # Собираем полный список всех товаров на главной странице
+    #     product_layout_list = self.get_elements(self.PRODUCT_LAYOUT, wait)
+    #     number = random.randint(0, len(product_layout_list) - 1)
+    #
+    #     # Выбираем случайный товар из имеющихся
+    #     random_product = self.get_elements(self.PRODUCT_LAYOUT, wait)[number]
+    #
+    #     # Сохраняем символ валюты случайно выбранного товара в переменную
+    #     product_currency_symbol = random_product.find_element(*self.PRODUCT_CURRENCY_SYMBOL).text[0]
+    #     return product_currency_symbol
+    #
+    # def change_currency(self, wait):
+    #     # Меняем валюту на EUR
+    #     self.get_element(self.CURRENCY_DROPDOWN_TOGGLE, wait).click()
+    #     self.get_element(self.CURRENCY_SELECT_EUR, wait).click()
+    #
+    #     # Проверяем валюту после её смены
+    #     # Собираем полный список всех товаров на главной странице
+    #     product_layout_list = self.get_elements(self.PRODUCT_LAYOUT, wait)
+    #     number = random.randint(0, len(product_layout_list) - 1)
+    #
+    #     # Выбираем случайный товар из имеющихся
+    #     random_product = self.get_elements(self.PRODUCT_LAYOUT, wait)[number]
+    #
+    #     # Сохраняем символ валюты случайно выбранного товара в переменную
+    #     product_currency_symbol = random_product.find_element(By.CSS_SELECTOR, "p.price").text.splitlines()[0][-1]
+    #     return product_currency_symbol
+
+    def get_random_product_currency_symbol(self, wait):
+        # Собираем полный список всех товаров на главной странице
+        product_layout_list = self.get_elements(self.PRODUCT_LAYOUT, wait)
+        number = random.randint(0, len(product_layout_list) - 1)
+        # Выбираем случайный товар из имеющихся
+        random_product = self.get_elements(self.PRODUCT_LAYOUT, wait)[number]
+        # Сохраняем символ валюты случайно выбранного товара в переменную
+        product_currency_symbol = random_product.find_element(*self.PRODUCT_CURRENCY_SYMBOL)
+        return product_currency_symbol
+
+    def get_default_currency(self, wait):
+        # Проверяем валюту по умолчанию
+        return self.get_random_product_currency_symbol(wait).text[0]
+
+    def change_currency(self, wait):
+        # Меняем валюту на EUR
+        self.get_element(self.CURRENCY_DROPDOWN_TOGGLE, wait).click()
+        self.get_element(self.CURRENCY_SELECT_EUR, wait).click()
+        # Проверяем валюту после её смены
+        return self.get_random_product_currency_symbol(wait).text.splitlines()[0][-1]
