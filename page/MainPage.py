0eaf53684ae378ab4d9195deb5fb95401bdd9661
Roman <dumgym@gmail.com>
2023-11-28 08:25:54 +0300

    Update MainPage.py - currency functions and get_random_product function

--- page/MainPage.py
+++ page/MainPage.py
@@ -82,23 +82,50 @@ class MainPage(BasePage):
     #     product_currency_symbol = random_product.find_element(By.CSS_SELECTOR, "p.price").text.splitlines()[0][-1]
     #     return product_currency_symbol
 
-    def get_random_product_currency_symbol(self, wait):
+    # def get_random_product_currency_symbol(self, wait):
+    #     # Собираем полный список всех товаров на главной странице
+    #     product_layout_list = self.get_elements(self.PRODUCT_LAYOUT, wait)
+    #     number = random.randint(0, len(product_layout_list) - 1)
+    #     # Выбираем случайный товар из имеющихся
+    #     random_product = self.get_elements(self.PRODUCT_LAYOUT, wait)[number]
+    #     # Сохраняем символ валюты случайно выбранного товара в переменную
+    #     product_currency_symbol = random_product.find_element(*self.PRODUCT_CURRENCY_SYMBOL)
+    #     return product_currency_symbol
+    #
+    # def get_default_currency(self, wait):
+    #     # Проверяем валюту по умолчанию
+    #     return self.get_random_product_currency_symbol(wait).text[0]
+    #
+    # def change_currency(self, wait):
+    #     # Меняем валюту на EUR
+    #     self.get_element(self.CURRENCY_DROPDOWN_TOGGLE, wait).click()
+    #     self.get_element(self.CURRENCY_SELECT_EUR, wait).click()
+    #     # Проверяем валюту после её смены
+    #     return self.get_random_product_currency_symbol(wait).text.splitlines()[0][-1]
+
+    def get_random_product(self, wait):
         # Собираем полный список всех товаров на главной странице
         product_layout_list = self.get_elements(self.PRODUCT_LAYOUT, wait)
         number = random.randint(0, len(product_layout_list) - 1)
         # Выбираем случайный товар из имеющихся
         random_product = self.get_elements(self.PRODUCT_LAYOUT, wait)[number]
-        # Сохраняем символ валюты случайно выбранного товара в переменную
-        product_currency_symbol = random_product.find_element(*self.PRODUCT_CURRENCY_SYMBOL)
-        return product_currency_symbol
+        return random_product
 
     def get_default_currency(self, wait):
-        # Проверяем валюту по умолчанию
-        return self.get_random_product_currency_symbol(wait).text[0]
+        # Выбираем случайный товар на странице
+        random_product = self.get_random_product(wait)
+        # Сохраняем символ валюты случайно выбранного товара в переменную
+        product_currency_symbol = random_product.find_element(*self.PRODUCT_CURRENCY_SYMBOL)
+        # Возвращаем символ валюты
+        return product_currency_symbol.text[0]
 
     def change_currency(self, wait):
         # Меняем валюту на EUR
         self.get_element(self.CURRENCY_DROPDOWN_TOGGLE, wait).click()
         self.get_element(self.CURRENCY_SELECT_EUR, wait).click()
-        # Проверяем валюту после её смены
-        return self.get_random_product_currency_symbol(wait).text.splitlines()[0][-1]
+        # Выбираем случайный товар на странице
+        random_product = self.get_random_product(wait)
+        # Сохраняем символ валюты случайно выбранного товара в переменную
+        product_currency_symbol = random_product.find_element(*self.PRODUCT_CURRENCY_SYMBOL)
+        # Возвращаем символ валюты
+        return product_currency_symbol.text.splitlines()[0][-1]
