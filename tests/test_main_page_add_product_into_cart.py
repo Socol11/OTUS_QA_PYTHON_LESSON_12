ca530f0aa8d9cfc88d729b9f494717b546251899
Roman <dumgym@gmail.com>
2023-11-28 18:10:15 +0300

    Littl update in test in test_main_page_add_product_into_cart.py

--- tests/test_main_page_add_product_into_cart.py
+++ tests/test_main_page_add_product_into_cart.py
@@ -1,8 +1,4 @@
 from page.MainPage import MainPage
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import random
 
 
 class TestMainPageAddProductIntoCart:
